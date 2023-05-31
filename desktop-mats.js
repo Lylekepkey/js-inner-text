4510b98dc26560822217843d377274ba12aea1f6
HOPELESSWONDER <104387577+HOPELESSWONDER@users.noreply.github.com>
2023-05-31 17:28:18 +0200

    not done

--- desktop-mats.js
+++ desktop-mats.js
@@ -2,39 +2,36 @@ const inner = [
 
     {
     image: "image/destiny_2_lightfall_lp_tile_d_tile_1_3.png__440x440_crop-scale_optimize_subsampling-2.webp",
-    year: 2019,
-    desc: "Latest certificate",
-    place: "This place",
+    name: "ARCTIS 7+ DESTINY 2: LIGHTFALL EDITION",
+        description: "Price: R3000",
     },
     {
     image: "image/qck_prism_xl_destiny2_lightfall_pdp_img_buy_01.png__1850x800_crop-scale_optimize_subsampling-2.webp",
-    year: 2019,
-    desc: "Latest certificate",
-    place: "This place",
+    name: "ARCTIS 7+ DESTINY 2: LIGHTFALL EDITION",
+        description: "Price: R3000",
     },
     {
     image: "image/qck_prism_xl_destiny2_lightfall_pdp_img_buy_02.png__1920x1080_crop-fit_optimize_subsampling-2.png",
-    year: 2019,
-    desc: "Latest certificate",
-    place: "This place",
+    name: "ARCTIS 7+ DESTINY 2: LIGHTFALL EDITION",
+    description: "Price: R3000",
     },
     {
     image: "image/qck_prism_xl_destiny2_lightfall_pdp_img_buy_03.png__1920x1080_crop-fit_optimize_subsampling-2.png",
-    year: 2019,
-    desc: "Latest certificate",
-    place: "This place",
+    name: "ARCTIS 7+ DESTINY 2: LIGHTFALL EDITION",
+    description: "Price: R3000",
     },
   
 ];
+
 
 let displayText = document.querySelector(".inner");
 inner.forEach((data) => {
   displayText.innerHTML += `
     <div class="card">
-      <h4 class="display-5">${data.year}</h4>
+      <h4 class="display-5">${data.name}</h4>
       <div class="card-body">
         <img src="${data.image}" style="width:140px;">
-        <p class="text-wheat">${data.desc} @ <span>${data.place}</span></p>
+        <p class="text-wheat">${data.description}</p>
       </div>
     </div>`;
 });
--- text.js
+++ text.js
@@ -1,51 +1,46 @@
 const inner = [
   {
     image: "image/aerox_5_destiny2_lightfall_pdp_img_buy_01.png__1850x800_crop-scale_optimize_subsampling-2.webp",
-    year: 2023,
-    desc: "Latest certificate",
-    place: "This place",
+    name: "ARCTIS 7+ DESTINY 2: LIGHTFALL EDITION",
+        description: "Price: R3000",
   },
   {
     image: "image/aerox_5_destiny2_lightfall_pdp_img_buy_02-1.png__1850x800_crop-scale_optimize_subsampling-2.webp",
-    year: 2023,
-    desc: "Latest certificate",
-    place: "This place",
+    name: "ARCTIS 7+ DESTINY 2: LIGHTFALL EDITION",
+        description: "Price: R3000",
   },
   {
     image: "image/aerox_5_destiny2_lightfall_pdp_img_buy_02-2.png__1850x800_crop-scale_optimize_subsampling-2.webp",
-    year: 2023,
-    desc: "Latest certificate",
-    place: "This place",
+    name: "ARCTIS 7+ DESTINY 2: LIGHTFALL EDITION",
+        description: "Price: R3000",
   },
   {
     image: "image/aerox_5_destiny2_lightfall_pdp_img_buy_03.png__1850x800_crop-scale_optimize_subsampling-2.webp",
-    year: 2023,
-    desc: "Latest certificate",
-    place: "This place",
+    name: "ARCTIS 7+ DESTINY 2: LIGHTFALL EDITION",
+        description: "Price: R3000",
   },
   {
     image: "image/aerox_5_destiny2_lightfall_pdp_img_buy_04.png__1850x800_crop-scale_optimize_subsampling-2.webp",
-    year: 2023,
-    desc: "Latest certificate",
-    place: "This place",
+    name: "ARCTIS 7+ DESTINY 2: LIGHTFALL EDITION",
+        description: "Price: R3000",
   },
   {
     image: "image/destiny_2_lightfall_lp_tile_d_tile_1_2.png__440x440_crop-scale_optimize_subsampling-2.webp",
-    year: 2023,
-    desc: "Latest certificate",
-    place: "This place",
+    name: "ARCTIS 7+ DESTINY 2: LIGHTFALL EDITION",
+        description: "Price: R3000",
   }
   
 ];
+
 
 let displayText = document.querySelector(".inner");
 inner.forEach((data) => {
   displayText.innerHTML += `
     <div class="card">
-      <h4 class="display-5">${data.year}</h4>
+      <h4 class="display-5">${data.name}</h4>
       <div class="card-body">
         <img src="${data.image}" style="width:140px;">
-        <p class="text-wheat">${data.desc} @ <span>${data.place}</span></p>
+        <p class="text-wheat">${data.description}</p>
       </div>
     </div>`;
 });
